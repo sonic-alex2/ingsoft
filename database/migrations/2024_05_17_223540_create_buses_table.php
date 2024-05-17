43a8b64bbeeb937a36fea56ca54578aed5a529ec
Jorge Ramos <jorgeramos@Jorges-MacBook-Pro.local>
2024-05-17 16:39:15 -0600

    Laravel solo con inicio de sesion y un migration

---
+++ database/migrations/2024_05_17_223540_create_buses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('buses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // nombre
+            $table->string('registration'); // matricula
+            $table->string('model'); // modelo
+            $table->string('manufacturer'); // fabricante
+            $table->integer('max_capacity'); // capacidad maxima
+            $table->boolean('notification')->default(false); // notificacion
+            $table->time('route_time'); // tiempo de la ruta
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('buses');
+    }
+};
